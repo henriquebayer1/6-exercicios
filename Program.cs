312bda0433982974070dc0a438da60da30d39bef
Henrique Jordão Baier De Azevedo <45922978810@info.local>
2023-03-30 10:58:39 -0300

    exercicios

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿//Exercicio 1
+
+// float salarioRecebido = 1200;
+
+// Console.WriteLine($"Informe o valor Gasto para calcular o orcamento:");
+// float valorGasto = float.Parse(Console.ReadLine());
+
+
+// float sobraDeOrcamento = (salarioRecebido - valorGasto);
+
+// Console.WriteLine($"a sobra de orcamento e de: {sobraDeOrcamento}");
+
+// if(sobraDeOrcamento < 0)
+// { Console.WriteLine($"Orcamento estorado, tu ta quebrado");
+
+// }
+// else
+// {Console.WriteLine($"Ta de boa, grana ta em sobra");
+// }
+
+
+
+
+
+
+
+
+//Exercicio 2
+
+
+// Console.WriteLine($"Informe o placar dos dois times:");
+
+// Console.WriteLine($"Time 1:");
+// int placartime1 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine($"Time 2:");
+// int placartime2 = int.Parse(Console.ReadLine());
+
+// if(placartime1 > placartime2)
+// {Console.WriteLine($"VITORIA DO TIME 1");
+// }
+// else if(placartime2 > placartime1)
+// {Console.WriteLine($"VITORIA DO TIME 2");
+
+    
+// }
+// else
+// {System.Console.WriteLine($"EMPATOU");
+    
+// }
+
+
+
+
+
+
+
+//Exercicio 3
+
+int equilatero = 3;
+int Isoscele = 2;
+int escaleno = 0;
+
+
+Console.WriteLine($"Informe quantos lados iguais tem o seu triangulo:");
+int ladosIguais = int.Parse(Console.ReadLine());
+
+
+
+if(equilatero == 3)
+{Console.WriteLine($"E um equilatero");
+}
+else if(Isoscele == 2)
+{Console.WriteLine($"E um Isoscele");
+}
+ else {System.Console.WriteLine($"E um escaleno");}
+
+
+
+
+
+
+
+
+//Exercicio 4
+
+// int senha = 1235;
+
+
+
+// if(senha == 1234)
+// {Console.WriteLine($"Permitir o acesso");
+
+// }
+// else
+// {Console.WriteLine($"Acesso Negado");
+
+    
+// }
+
+
+
+
+
+//Exercicio 5
+
+
+// float macasMenosUmaduzia = 0.30f;
+// float macasUmaduzia = 0.25f;
+
+// if(macasMenosUmaduzia)
+// {}
+
+
+
+
+
+
+
+//Exercicio 6
+
+
+
+
